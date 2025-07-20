cb97cfe930b1ea38897b35e7d8a69eeb8f7a7f86
xcalder <xcalder@foxmail.com>
2025-07-20 18:48:59 +0800

    后台首页报表

---
+++ lang/en/manager.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'stats_users' => 'Registrations Today',
+    'stats_orders' => 'Orders Today',
+    'stats_cart_items' => 'Add to Cart Today',
+    'stats_today' => 'Today',
+    'stats_yesterday' => 'Yesterday',
+];
