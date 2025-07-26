783dc3f3c9b22898441cd97f32597d4e1cc30208
xcalder <xcalder@foxmail.com>
2025-07-26 13:47:45 +0800

    01235

--- app/Models/AttributeValue.php
+++ app/Models/AttributeValue.php
@@ -6,7 +6,9 @@
 
 namespace App\Models;
 
+use App\Observers\AttributeValueObserver;
 use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Attributes\ObservedBy;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -28,6 +30,9 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
  *
  * @package App\Models
  */
+
+#[ObservedBy([AttributeValueObserver::class])]
+
 class AttributeValue extends Model
 {
     use HasFactory;
---
+++ app/Observers/AttributeValueObserver.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\AttributeValue;
+
+class AttributeValueObserver
+{
+    /**
+     * 清除所有语言下的属性缓存
+     */
+    protected function clearAttributeCache(): void
+    {
+        \Illuminate\Support\Facades\Cache::forget("attributes.with.translations");
+    }
+
+    /**
+     * Handle the AttributeValue "created" event.
+     */
+    public function created(AttributeValue $attributeValue): void
+    {
+        //
+        $this->clearAttributeCache();
+    }
+
+    /**
+     * Handle the AttributeValue "updated" event.
+     */
+    public function updated(AttributeValue $attributeValue): void
+    {
+        //
+        $this->clearAttributeCache();
+    }
+
+    /**
+     * Handle the AttributeValue "deleted" event.
+     */
+    public function deleted(AttributeValue $attributeValue): void
+    {
+        //
+        $this->clearAttributeCache();
+    }
+}
