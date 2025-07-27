5e2036df4b6d182cf02c4e4121597bcb7dd22737
xcalder <xcalder@foxmail.com>
2025-07-27 23:48:28 +0800

    0123

--- sync_storage.sh
+++ sync_storage.sh
@@ -4,12 +4,13 @@ LOCAL_DIR="/home/wwwroot/teanary.test/shared/storage/app/"
 REMOTE_DIR="/home/wwwroot/teanary/shared/storage/app"
 REMOTE_HOST="your.remote.ip.or.host"
 REMOTE_USER="root"
+SSH_KEY="/home/youruser/.ssh/id_rsa"  # 替换为你的私钥路径
 
-# Rsync local to remote
-rsync -az --delete "${LOCAL_DIR}" "${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_DIR}/"
+# Rsync local to remote with SSH key
+rsync -az -e "ssh -i ${SSH_KEY}" --delete "${LOCAL_DIR}" "${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_DIR}/"
 
 # Change owner and permission remotely
-ssh ${REMOTE_USER}@${REMOTE_HOST} <<EOF
+ssh -i ${SSH_KEY} ${REMOTE_USER}@${REMOTE_HOST} <<EOF
 chown -R www:www ${REMOTE_DIR}
 chmod -R 777 ${REMOTE_DIR}
 EOF
