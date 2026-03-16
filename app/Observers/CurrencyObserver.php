cefc131daaef983062836b521e3da9865aab276d
Xcalder <xcalder@foxmail.com>
2026-03-16 13:32:33 +0800

    修改货币后更新汇率

--- app/Observers/CurrencyObserver.php
+++ app/Observers/CurrencyObserver.php
@@ -4,12 +4,14 @@ namespace App\Observers;
 
 use App\Models\Currency;
 use App\Services\LocaleCurrencyService;
+use Illuminate\Support\Facades\Artisan;
 
 class CurrencyObserver
 {
     public function saved(Currency $currency)
     {
         (new LocaleCurrencyService)->clearCurrenciesCache();
+        Artisan::call('app:update-ecb');
     }
 
     public function deleted(Currency $currency)
